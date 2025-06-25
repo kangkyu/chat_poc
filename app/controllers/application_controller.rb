002b05d1898223ccca79ab9d3f2cfb251da7327c
Kang-Kyu Lee <kangkyu1111@gmail.com>
2025-06-24 22:26:05 -0700

    user login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,20 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
+
+  def require_signin
+    redirect_to new_session_url unless current_user_id.present?
+  end
+
+  def require_logout
+    redirect_to root_url if current_user_id.present?
+  end
+
+  def current_user
+    @current_user ||= User.find_by(id: current_user_id)
+  end
+
+  def current_user_id
+    session[:user_id]
+  end
 end
---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,23 @@
+class SessionsController < ApplicationController
+  before_action :require_signin, only: [:destroy]
+  before_action :require_logout, only: [:new, :create]
+
+  def new
+  end
+
+  def create
+    user = User.find_by(username: params[:username])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect_to root_url, notice: "Login successful"
+    else
+      flash.now.alert = "Invalid username or password"
+      render 'new'
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    redirect_to new_session_url, notice: "You're now signed out!"
+  end
+end
